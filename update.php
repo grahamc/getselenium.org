b347220d5cc6b4f94e0a9f5bc02dacebb1a08319
Graham Christensen <gchristensen@nationalfield.org>
2011-12-20 23:33:50 -0500

    Get Selenium! wget http://getselenium.org

---
+++ update.php
@@ -0,0 +1,11 @@
+<?php
+
+$atom = file_get_contents('http://code.google.com/feeds/p/selenium/downloads/basic');
+
+$startPos = strpos($atom, 'http://selenium.googlecode.com/files/selenium-server-standalone');
+$atom = substr($atom, $startPos);
+$endPos = strpos($atom, '.jar');
+$atom = substr($atom, 0, $endPos + 4);
+
+$out = "<?php header('Location: $atom', true, 302); ?>";
+file_put_contents('web/index.php', $out);
